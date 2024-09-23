de82dcae88dced51ba2e601dc588a6da29353266
Juniplay <juniplay2016@gmail.com>
2024-09-22 21:50:28 -0300

    Adicionar arquivos de projeto.

---
+++ Models/AplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Globalization;
+
+namespace Blog.Models
+{
+    public class AplicationUser:IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        //relacionado ao Post
+        public List<Post> Posts { get; set; }
+    }  
+}
---
+++ Models/Post.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Blog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }  
+        //relacionado ao AplicationUser
+        public string? AplicationUserId { get; set; }
+        public AplicationUser? AplicationUser { get; set; }
+        public DateTime? PostCreat { get; set; } = DateTime.Now;
+
+        public string? Description { get; set; }    
+        public string? Slug { get; set; }  
+    }
+}
